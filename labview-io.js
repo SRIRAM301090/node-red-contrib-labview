1c9d196fab69b18e462c076956b255e22e7ad1ea
Sriram Gopalan <shriramgopalan@gmail.com>
2022-12-10 23:18:37 +0530

    Add Event Handler to LabVIEW Input Node

--- labview-io.js
+++ labview-io.js
@@ -1,38 +1,49 @@
 module.exports = function (RED) {
-  // var udp = require("dgram");
-  // var socket = udp.createSocket("udp4");
-  
+  const udp = require("dgram");
+  const events = require("events");
+  const eventEmitter = new events.EventEmitter();
+
   function lvConfig(n) {
     RED.nodes.createNode(this, n);
+
     this.listeningPort = n.listeningPort;
     this.host = n.host;
     this.port = n.port;
 
-    // socket.on('message', function (msg, info){
-    //     console.log(msg.toString());
-    //  });
-    
-    // socket.on('listening', function(){
-    //     var address = socket.address();
-    //     console.log("listening on :" = address.address + ":" + address.port);
-    // });
-    
-    // socket.bind(this.listeningPort);
+    const server = udp.createSocket("udp4");
+    server.bind(this.listeningPort);
+    server.on("message", function (msg, info) {
+      const payload = JSON.parse(msg.toString());
+      console.log(payload);
+      eventEmitter.emit(payload.topic, payload);
+    });
+
+    //emits after the socket is closed using socket.close();
+    server.on("close", function () {
+      console.log("Socket is closed !");
+    });
   }
   RED.nodes.registerType("lv-config", lvConfig);
 
   function lvInput(n) {
     RED.nodes.createNode(this, n);
-
+    var node = this;
+    this.topic = n.topic;
     this.lvConfig = RED.nodes.getNode(n.lvConfig);
+    var msg = {};
     console.log(this.lvConfig);
 
-    if(this.lvConfig) {
-        console.log(this.lvConfig.listeningPort);
-        console.log(this.lvConfig.host);
-        console.log(this.lvConfig.port);
+    if (this.lvConfig) {
+      eventEmitter.removeAllListeners(this.topic);
+      eventEmitter.addListener(this.topic, function(data) {
+        msg.payload = data.message;
+        node.send(msg);
+    })
     }
+
+    node.on("close", function () {
+      eventEmitter.removeListener(this.topic)
+    });
   }
   RED.nodes.registerType("lv-input", lvInput);
-
 };
