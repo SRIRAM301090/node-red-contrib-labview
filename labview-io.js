e186da4da784abd82deb1172772f00e36affa3b9
Sriram Gopalan <shriramgopalan@gmail.com>
2022-12-11 09:14:04 +0530

    Add Error Handling

--- labview-io.js
+++ labview-io.js
@@ -5,25 +5,32 @@ module.exports = function (RED) {
 
   function labviewConfig(n) {
     RED.nodes.createNode(this, n);
+    var node = this;
     this.listeningPort = n.listeningPort;
     this.host = n.host;
     this.port = n.port;
 
     // creating a udp server
     const server = udp.createSocket("udp4");
-    server.bind(this.listeningPort);
+    if (this.listeningPort) {
+      try {
+        server.bind(this.listeningPort);
 
-    // emits on new datagram msg
-    server.on("message", function (msg, info) {
-      const payload = JSON.parse(msg.toString());
-      console.log(payload);
-      eventEmitter.emit(payload.topic, payload);
-    });
+        // emits on new datagram msg
+        server.on("message", function (msg, info) {
+          const payload = JSON.parse(msg.toString());
+          console.log(payload);
+          eventEmitter.emit(payload.topic, payload);
+        });
 
-    //emits after the socket is closed using socket.close();
-    server.on("close", function () {
-      console.log("Socket is closed !");
-    });
+        //emits after the socket is closed using socket.close();
+        server.on("close", function () {
+          console.log("Socket is closed !");
+        });
+      } catch (err) {
+        node.error(err);
+      }
+    }
   }
   RED.nodes.registerType("labview-config", labviewConfig);
 
@@ -41,11 +48,16 @@ module.exports = function (RED) {
     form = form.split("|")[0];
 
     if (this.labviewConfig) {
-      eventEmitter.removeAllListeners(this.topic);
-      eventEmitter.addListener(this.topic, function (data) {
-        msg.payload = RED.util.getMessageProperty(data, form);
-        node.send(msg);
-      });
+      try {
+        eventEmitter.removeAllListeners(this.topic);
+        eventEmitter.addListener(this.topic, function (data) {
+          msg.payload = RED.util.getMessageProperty(data, form);
+          node.send(msg);
+        });
+      } catch (err) {
+        console.log("ErrorMessage",err);
+        node.error(err);
+      }
     }
 
     node.on("close", function () {
